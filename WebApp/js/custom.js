1b3bd200ccd1a2a378d80fb1b6a18ec3d96f893e
ajith23 <23ajith7@gmail.com>
2016-02-03 09:48:30 -0700

    new charts added

--- WebApp/js/custom.js
+++ WebApp/js/custom.js
@@ -9,6 +9,7 @@
             $('#userQuery').html("");
             var value = d3.select(this).property("value");
             fetchSelectedUserIntentionChartData(value);
+            fetchUserOperationTimeSeriesData(value);
             //alert(value);
         });
 
@@ -22,25 +23,37 @@
 
     fetchAllUserIntentionChartData();
     fetchSelectedUserIntentionChartData('A0001');
-    //fetchUserOperationTimeSeriesData();
+    fetchUserOperationTimeSeriesData('A0001');
     //fetchUrlClickCountScatterChartData();
     //generateBubbleChart();
-    //fetchAndLoadOperationSummaryChartData();
+    generateUserClickCountReport();
+    fetchOperationSummaryChartData();
 });
 
-function generateAllUserIntentionChart(chartData)
-{
+function generateAllUserIntentionChart(chartData) {
     var chart1 = c3.generate({
         bindto: '#chartIntentionAll',
         data: {
             columns: chartData,
             type: 'pie',
+            order: null,
             onclick: function (d, i) {
                 console.log("onclick", d, i);
             },
             onmouseover: function (d, i) { console.log("onmouseover", d, i); },
             onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        order: null,
+        legend: {
+            position: 'right'
         }
+        //pie: {
+        //    label: {
+        //        format: function (value, ratio, id) {
+        //            return id + ":" + ratio;
+        //        }
+        //    }
+        //}
     });
 }
 
@@ -50,13 +63,25 @@ function generateSelectedUserIntentionChart(chartData) {
         data: {
             columns: chartData,
             type: 'pie',
+            order: null,
             onclick: function (d, i) {
                 console.log("onclick", d, i);
                 fetchAndLoadSelectedUserQueries($("#userListDiv option:selected").text(), d.id);
             },
             onmouseover: function (d, i) { console.log("onmouseover", d, i); },
             onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        order: null,
+        legend: {
+            position: 'right'
         }
+        //pie: {
+        //    label: {
+        //        format: function (value, ratio, id) {
+        //            return id + ":" + ratio;
+        //        }
+        //    }
+        //}
     });
 
     chart2.load({
@@ -64,12 +89,10 @@ function generateSelectedUserIntentionChart(chartData) {
     });
 }
 
-function fetchAllUserIntentionChartData()
-{
+function fetchAllUserIntentionChartData() {
     d3.csv("data/queryOutput/intentionOverall.csv", function (error, data) {
         var columnArray = [];
-        for(var i=0;i<data.length; i++)
-        {
+        for (var i = 0; i < data.length; i++) {
             var cellArray = [];
             cellArray[0] = data[i].intention;
             cellArray[1] = data[i].intentionCount;
@@ -79,8 +102,7 @@ function fetchAllUserIntentionChartData()
     });
 }
 
-function fetchSelectedUserIntentionChartData(userId)
-{
+function fetchSelectedUserIntentionChartData(userId) {
     d3.csv("data/queryOutput/intentionUserSpecific.csv", function (error, data) {
         var columnArray = [];
         var index = 0;
@@ -97,17 +119,14 @@ function fetchSelectedUserIntentionChartData(userId)
     });
 }
 
-function fetchAndLoadSelectedUserQueries(userId, intention)
-{
-    d3.csv("data/class_query_transformed.csv", function (error, data) {
+function fetchAndLoadSelectedUserQueries(userId, intention) {
+    d3.csv("data/queryOutput/intentionQuery.csv", function (error, data) {
         var htmlString = "<table>";
         htmlString += '<tr><td><h4> User Queries for ' + intention + '</h4></td></tr>';
         var index = 0;
-        for (var i = 0; i < data.length; i++)
-        {
-            if (((data[i].u_id).localeCompare(userId) == 0) && ((data[i].intention).localeCompare(intention) == 0))
-            {
-                htmlString += '<tr><td>' +  data[i].query + '</td></tr>';
+        for (var i = 0; i < data.length; i++) {
+            if (((data[i].u_id).localeCompare(userId) == 0) && ((data[i].intention).localeCompare(intention) == 0)) {
+                htmlString += '<tr><td>' + data[i].query + '</td></tr>';
             }
         }
         htmlString += "</table>";
@@ -115,7 +134,7 @@ function fetchAndLoadSelectedUserQueries(userId, intention)
     });
 }
 
-function fetchAndLoadOperationSummaryChartData() {
+function fetchOperationSummaryChartData() {
     d3.csv("data/queryOutput/urlOperationSummary.csv", function (error, data) {
         var columnArray = [];
         var index = 0;
@@ -132,7 +151,7 @@ function fetchAndLoadOperationSummaryChartData() {
         cellArrayScrollDown[0] = 'ScrollDown';
         cellArrayScrollUp[0] = 'ScrollUp';
         cellArrayTargetClicked[0] = 'TargetClicked';
-        cellArraySelect[0]='Select';
+        cellArraySelect[0] = 'Select';
 
         xScrollDown[0] = 'x1';
         xScrollUp[0] = 'x2';
@@ -174,8 +193,7 @@ function fetchAndLoadOperationSummaryChartData() {
     });
 }
 
-function generateUrlOperationSummaryChart(chartData)
-{
+function generateUrlOperationSummaryChart(chartData) {
     var chart = c3.generate({
         bindto: '#urlOperationSummaryChart',
         data: {
@@ -197,6 +215,14 @@ function generateUrlOperationSummaryChart(chartData)
         subchart: {
             show: true
         },
+        grid: {
+            x: {
+                show: true
+            },
+            y: {
+                show: true
+            }
+        },
         axis: {
             x: {
                 type: 'category'
@@ -210,15 +236,33 @@ function generateUserOperationTimeSeriesChart(chartData) {
     var chart = c3.generate({
         bindto: '#chartUserOperationTimeSeries',
         data: {
-            x: 'timestamp_actual',
-             xFormat: '%Y-%m-%d %H:%M:%S.000' , // 'xFormat' can be used as custom format of 'x'
-            columns: chartData
+            xs: {
+                'ScrollDown': 'ScrollDownTime',
+                'ScrollUp': 'ScrollUpTime',
+                'TargetClicked': 'TargetClickedTime',
+                'Select': 'SelectTime',
+            },
+            //x: 'ScrollDown',
+            xFormat: '%Y-%m-%d %H:%M:%S.000', // 'xFormat' can be used as custom format of 'x'
+            columns: chartData,
+            type: 'line',
+            //groups: [
+            //['ScrollDown', 'ScrollUp']
+            //]
         },
         zoom: {
             enabled: true
         },
-        subchart: {
-            show: true
+        //subchart: {
+        //    show: true
+        //},
+        grid: {
+            x: {
+                show: true
+            },
+            y: {
+                show: true
+            }
         },
         axis: {
             x: {
@@ -229,22 +273,71 @@ function generateUserOperationTimeSeriesChart(chartData) {
             }
         }
     });
+    chart.load({
+        columns: chartData
+    });
 }
 
-function fetchUserOperationTimeSeriesData() {
-    d3.csv("data/queryOutput/userOperationWRTTime.csv", function (error, data) {
+function fetchUserOperationTimeSeriesData(userId) {
+    d3.csv("data/queryOutput/userOperationTimeline.csv", function (error, data) {
         var columnArray = [];
         var cellArray1 = [];
         var cellArray2 = [];
         cellArray1[0] = 'timestamp_actual';
         cellArray2[0] = 'operationCount';
+        
+        var sdCountArray = [];
+        var sdTimeArray = [];
+        var suCountArray = [];
+        var suTimeArray = [];
+        var tcCountArray = [];
+        var tcTimeArray = [];
+        var sCountArray = [];
+        var sTimeArray = [];
+
+        sdCountArray[0] = 'ScrollDown'
+        sdTimeArray[0] = 'ScrollDownTime'
+        suCountArray[0] = 'ScrollUp'
+        suTimeArray[0] = 'ScrollUpTime'
+        tcCountArray[0] = 'TargetClicked'
+        tcTimeArray[0] = 'TargetClickedTime'
+        sCountArray[0] = 'Select'
+        sTimeArray[0] = 'SelectTime'
+
         for (var i = 0; i < data.length; i++) {
-            cellArray1[i+1] = data[i].timestamp_actual;
-            cellArray2[i+1] = data[i].operationCount;
+            if ((data[i].u_id).localeCompare(userId) == 0) {
+                //cellArray1[cellArray1.length] = data[i].timestamp_actual;
+                //cellArray2[cellArray2.length] = data[i].operationCount;
+
+                if ((data[i].operation).localeCompare('scroll_down') == 0) {
+                    sdTimeArray[sdTimeArray.length] = data[i].timestamp_actual;
+                    sdCountArray[sdCountArray.length] = data[i].operationCount;
+                }
+                else if ((data[i].operation).localeCompare('scroll_up') == 0) {
+                    suTimeArray[suTimeArray.length] = data[i].timestamp_actual;
+                    suCountArray[suCountArray.length] = data[i].operationCount;
+                }
+                else if ((data[i].operation).localeCompare('target_clicked') == 0) {
+                    tcTimeArray[tcTimeArray.length] = data[i].timestamp_actual;
+                    tcCountArray[tcCountArray.length] = data[i].operationCount;
+                }
+                else if ((data[i].operation).localeCompare('select') == 0) {
+                    sTimeArray[sTimeArray.length] = data[i].timestamp_actual;
+                    sCountArray[sCountArray.length] = data[i].operationCount;
+                }
+            }
         }
-        columnArray[0] = cellArray1;
-        columnArray[1] = cellArray2;
+        columnArray[0] = sdTimeArray;
+        columnArray[1] = suTimeArray;
+        columnArray[2] = tcTimeArray;
+        columnArray[3] = sTimeArray;
+        columnArray[4] = sdCountArray;
+        columnArray[5] = suCountArray;
+        columnArray[6] = tcCountArray;
+        columnArray[7] = sCountArray;
+        
         generateUserOperationTimeSeriesChart(columnArray);
+        $('#chartUserOperationTimeSeriesHeader').html('Opeartion History for ' + userId);
     });
 }
 
@@ -255,7 +348,7 @@ function generateUrlClickCountScatterChart(chartData) {
             columns: chartData,
             type: 'scatter'
         }
-        
+
     });
 }
 
@@ -272,18 +365,17 @@ function fetchUrlClickCountScatterChartData() {
     });
 }
 
-//------------------------------------------------------------------------
 
-function generateBubbleChart()
-{
+function generateUserClickCountReport() {
     var diameter = 600,
     format = d3.format(",d"),
     color = d3.scale.category20c();
 
     var pack = d3.layout.pack()
-        .size([diameter, diameter])
-        .padding(1.5)
-        .value(function (d) { return d.clickCount; });
+        .sort(null)
+    .size([diameter, diameter])
+    .padding(1.5)
+    .value(function (d) { return d.userClickCount; });
 
     var vis = d3.select("#svgid").append("svg")
         .attr("width", diameter)
@@ -292,18 +384,73 @@ function generateBubbleChart()
       .append("g");
 
 
-    d3.csv("data/queryOutput/urlClickCountSummary.csv", function (csvData) {
-        // put csv into a data structure pack layout will accept
+    d3.csv("data/queryOutput/userClickCountReport.csv", function (csvData) {
         var data = { name: "operation", children: csvData };
-
         var node = vis.data([data]).selectAll("circle")
-            .data(pack.nodes)
-          .enter().append("circle")
-            .attr("class", "node")
-            .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; })
-            .attr("r", function (d) { return d.r; })
-            .style("fill", function (d) { return color(d.operation); });
+        .data(pack.nodes)
+        .enter().append("circle")
+        .attr("class", "node")
+        .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; })
+        .attr("r", function (d) { return d.r; })
+        .style("fill", function (d) { return color(d.operation); })
+        .on("mouseover", function (d) {
+            generateSelectedUserClickCountReportGuage(d.operation, d.userClickCount, d.u_id);
 
-        node.append("title").text(function (d) { return d.url + ": " + format(d.clickCount); });
+            d3.csv("data/queryOutput/userClickCountReport.csv", function (data12) {
+                for (var i = 0; i < data12.length; i++) {
+                    if (((data12[i].u_id).localeCompare(d.u_id) == 0) && ((data12[i].operation).localeCompare('target_clicked') == 0)) {
+                        generateSelectedUserClickCountReportGuage('target_clicked', data12[i].userClickCount, d.u_id);
+                    }
+                    if (((data12[i].u_id).localeCompare(d.u_id) == 0) && ((data12[i].operation).localeCompare('select') == 0)) {
+                        generateSelectedUserClickCountReportGuage('select', data12[i].userClickCount, d.u_id);
+                    }
+                    if (((data12[i].u_id).localeCompare(d.u_id) == 0) && ((data12[i].operation).localeCompare('scroll_up') == 0)) {
+                        generateSelectedUserClickCountReportGuage('scroll_up', data12[i].userClickCount, d.u_id);
+                    }
+                    if (((data12[i].u_id).localeCompare(d.u_id) == 0) && ((data12[i].operation).localeCompare('scroll_down') == 0)) {
+                        generateSelectedUserClickCountReportGuage('scroll_down', data12[i].userClickCount, d.u_id);
+                    }
+                }
+            });
+
+        });
+
+        node.append("title").text(function (d) { return d.u_id + ": " + format(d.userClickCount); });
     });
+}
+
+function generateSelectedUserClickCountReportGuage(operation, userClickCount, userId)
+{
+    var average =0.0;
+    if (operation == 'target_clicked') average = 53.61
+    else if (operation == 'scroll_up') average = 1811.71
+    else if (operation == 'select') average = 13.71
+    else average = 6713.14
+    var d = userClickCount / average * 100.0
+    var chart1 = c3.generate({
+        bindto: '#' + operation + 'GuageChart',
+        data: {
+            columns: [['data', d]],
+            type: 'gauge',
+            onclick: function (d, i) { console.log("onclick", d, i); },
+            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+            onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        gauge: {
+            label: {
+                show: false
+            } // to turn off the min/max labels.
+        },
+        color: {
+            pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+            threshold: {
+                unit: 'value',
+                values: [30, 60, 90, 100]
+            }
+        },
+        size: {
+            height: 100
+        }
+    });
+    $('#userContributionDetailsHeader').html('User ' + userId + ' details- ')
 }
